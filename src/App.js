52520b17750b9e62c3e387f16cdd2789ef0efdca
nitesh852 <niteshbhatti852@gmail.com>
2023-04-19 13:21:37 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>nitesh</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNKoBGzp6zXSDxYLn-2I22bTYjpNYADTs",
+    authDomain: "clone-eb455.firebaseapp.com",
+    projectId: "clone-eb455",
+    storageBucket: "clone-eb455.appspot.com",
+    messagingSenderId: "185739855552",
+    appId: "1:185739855552:web:060c10dadbff177e290f25",
+    measurementId: "G-EEX2HC0Q40"
+  };
